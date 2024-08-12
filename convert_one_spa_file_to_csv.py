788cf0b3ae6d5631edf9e290fba6cf4de4a77c9c
DSinausia <119315272+sinausia@users.noreply.github.com>
2024-08-12 13:36:19 +0300

    typo

--- convert_one_spa_file_to_csv.py
+++ convert_one_spa_file_to_csv.py
@@ -2,14 +2,14 @@ import spectrochempy as scp
 import pandas as pd
 import numpy as np
 
-spa_file_path = r'"The directory of your spa file"'
-csv_file_path = r'"The directory of your spa file"\"the name of your csv file".csv'
+spa_file_path = r'/Users/danielsinausia/Downloads/DS_00145_01.SPA'
+csv_file_path = r'/Users/danielsinausia/Downloads/DS_00145_01.csv'
 
 dataset = scp.read(spa_file_path)
 data = dataset.data.T
 df = pd.DataFrame(data, columns=dataset.x_labels)
 
-wavenumbers = np.linspace((4000.1885, 649.9040, 6950))
+wavenumbers = np.linspace(4000.1885, 649.9040, 6950)
 df.insert(0, 'Wavenumber', wavenumbers)
 
 df.to_csv(csv_file_path, index=False, header=False)
